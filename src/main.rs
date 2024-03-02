12fcf8cbd43b0eb6250625117ef495e95f9ca692
GriHigley <higley.griffin@gmail.com>
2024-03-01 21:30:09 -0600

    adding more comments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+// Used to bring into scope
+// Could use to str::io::stdin in a inline context
 use std::io;
+// Method that will be execuded first for any rust program
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("You guessed: {guess}");    
+	// Macro for printing a line to concole. 	
+	// Macros are denoted by !
+	println!("Guess the number!");
+	println!("Please input your guess.");
+	// mut notes that this variable is mutable, by defualt all rust variables are imutable
+	// String::new() assigns a new empty string
+	let mut guess = String::new();
+	// reades in the value from the console
+	// &mut is used to point to the guess mutable variable 
+	io::stdin().read_line(&mut guess).expect("Failed to read line");
+	println!("You guessed: {guess}");    
 }
